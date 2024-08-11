04297c54537bfd9dfe619c44cc28e6672d49a5b8
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-08-11 11:53:27 +0530

    Update __init__.py

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -4,7 +4,6 @@ from AnonXMusic.core.git import git
 from AnonXMusic.core.userbot import Userbot
 from AnonXMusic.misc import dbb, heroku
 
-from SafoneAPI import SafoneAPI
 from .logging import LOGGER
 
 dirr()
@@ -14,7 +13,6 @@ heroku()
 
 app = Anony()
 userbot = Userbot()
-api = SafoneAPI()
 
 
 from .platforms import *
@@ -26,6 +24,3 @@ Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
-YTB = YTM()
-
-APP = "BRANDED_KUDI_BOT"  # connect music api key "Dont change it"
